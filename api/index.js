93ab838008910b188bae013954db64703a66b1a1
Heri Wahyudiono <heriwahyudiono.ris@gmail.com>
2025-07-11 08:44:44 +0700

    first commit

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const paymentRoutes = require('./routes/paymentRoutes');
+const paymentRoutes = require('../routes/paymentRoutes');
 
 const app = express();
 
-// Izinkan semua origin
-app.use(cors());
+const corsOptions = {
+  origin: ['https://globalplatform.vercel.app'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions)); // penting!
 app.use(express.json());
 app.use('/api/payments', paymentRoutes);
 
-module.exports = app; // untuk deploy di Vercel
+module.exports = app; // untuk Vercel, ekspor app, bukan listen()
 // app.listen(5000, () => {
 //   console.log('✅ Server running on port 5000');
 // });
